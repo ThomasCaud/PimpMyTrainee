5f2ce9f98cda2cec4ebb4260565d1379a950343d
Maxime Lucas <maxime.lucas96@gmail.com>
2018-10-14 11:38:42 +0200

    Update 2018-10-14_Creation de comptes.sql
    Suppression de la contrainte d'unicité sur 'id' car déjà contrainte de clé primaire
    Renommage de createDate en creationDate (oui oui)
    Changement du charset en utf8 avec collation utf8_general_ci

--- src/sql/2018-10-14_Creation de comptes.sql
+++ src/sql/2018-10-14_Creation de comptes.sql
@@ -6,10 +6,9 @@ CREATE TABLE `users` (
   `password` text NOT NULL,
   `company` text,
   `phone` text,
-  `createDate` date NOT NULL,
+  `creationDate` datetime NOT NULL,
   `isActive` tinyint(1) NOT NULL,
   `role` enum('admin','trainee') NOT NULL,
   PRIMARY KEY (`id`),
-  UNIQUE KEY `id` (`id`),
   UNIQUE KEY `email` (`email`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci
