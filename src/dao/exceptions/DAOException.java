3f1ad804c6f1715f0495ab5cc65c6894a4eb3618
Maxime LUCAS <maxime.lucas@equensworldline.com>
2018-10-18 17:05:40 +0200

    [MLS] Creation des classes pour les exceptions DAO

---
+++ src/dao/exceptions/DAOException.java
@@ -0,0 +1,21 @@
+package dao.exceptions;
+
+public class DAOException extends RuntimeException {
+
+	public DAOException(String arg0) {
+		super(arg0);
+	}
+
+	public DAOException(Throwable arg0) {
+		super(arg0);
+	}
+
+	public DAOException(String arg0, Throwable arg1) {
+		super(arg0, arg1);
+	}
+
+	public DAOException(String arg0, Throwable arg1, boolean arg2, boolean arg3) {
+		super(arg0, arg1, arg2, arg3);
+	}
+
+}
