735114fbb210d03a22e060a2fb4785f755d5fa5e
Maxime LUCAS <maxime.lucas@equensworldline.com>
2018-10-18 15:34:33 +0200

    [MLS] Adaptation du traitement du formulaire de login en mode MVC

--- src/models/forms/LoginForm.java
+++ src/models/forms/LoginForm.java
@@ -1,32 +1,34 @@
 package models.forms;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import javax.servlet.http.HttpServletRequest;
+import models.beans.User;
 
 public class LoginForm extends Form {
 	// Variables that represents each field of the form
 	private static final String FIELD_EMAIL = "email";
 	private static final String FIELD_PASSWORD = "password";
-	private static final String FIELD_PASSWORD_CONFIRM = "password_confirm";
 	
 	// Main method called by the servlet to process the login
-	public void connectUser(HttpServletRequest request) {
+	public User connectUser(HttpServletRequest request) {
 		String email = getFieldValue(request,FIELD_EMAIL);
 		String password = getFieldValue(request,FIELD_PASSWORD);
-		String password_confirm = getFieldValue(request,FIELD_PASSWORD_CONFIRM);
+		
+		User user = new User();
 		
 		try {
 			validateEmail(email);
 		} catch (Exception e) {
 			setError(FIELD_EMAIL, e.getMessage());
 		}
+		user.setEmail(email);
 		
 		try {
 			validatePassword(password);
 		} catch (Exception e) {
 			setError(FIELD_PASSWORD, e.getMessage());
 		}
+		user.setPassword(password);
+		
+		return user;
 	}
 }
