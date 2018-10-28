2224e887a94b5e3d697847b7cc8344759fad35c3
Thomas Caudrelier <thomas.caudrelier@etu.utc.fr>
2018-10-28 17:00:12 +0100

    Début de la recherche par utilisateur

--- src/controllers/admin/UsersController.java
+++ src/controllers/admin/UsersController.java
@@ -21,16 +21,22 @@ public class UsersController extends HttpServlet {
 
 	private static final long serialVersionUID = 1L;
 	private static final String VIEW = "/WEB-INF/admin_users_management.jsp";
+	private static final String FIELD_SEARCH = "searchByNameOrLastname";
 	private static final String ATT_USERS = "users";
 	private UserDAO userDAO;
-	
+
 	public void init() throws ServletException {
         this.userDAO = ( (DAOFactory) getServletContext().getAttribute( Config.CONF_DAO_FACTORY ) ).getUserDAO();
 	}
-	
-	public void doGet( HttpServletRequest request, HttpServletResponse response )	throws ServletException, IOException {
+
+	public void doGet(HttpServletRequest request, HttpServletResponse response)	throws ServletException, IOException {
 		HttpSession session = request.getSession();
+		String nameOrLastnameSearch = request.getParameter(FIELD_SEARCH);
+		// todo bien récupérer cette information
+		// + modifier le DAO pour que la recherche se fasse sur le nom et prénom
 		
+		System.out.println(nameOrLastnameSearch);
+
 		User user = (User) session.getAttribute(Config.ATT_SESSION_USER);
 		
 		if(user == null || user.getRole() != E_Role.ADMIN ) {
@@ -39,7 +45,7 @@ public class UsersController extends HttpServlet {
 		}
 		
 		ArrayList<User> users = userDAO.findAllUsers();
-		
+
 		request.setAttribute(ATT_USERS, users);
 		
 		this.getServletContext().getRequestDispatcher( VIEW ).forward( request, response );
