4890d79bd39bbbdc030136b0f93a9881fc14e750
Thomas Caudrelier <dev.caudrelier@gmail.com>
2018-10-29 14:33:38 +0100

    Update 2018-10-29_Ajout de plusieurs utilisateurs.sql

--- src/sql/2018-10-29_Ajout de plusieurs utilisateurs.sql
+++ src/sql/2018-10-29_Ajout de plusieurs utilisateurs.sql
@@ -2,33 +2,8 @@ INSERT INTO
 	`pimpmytrainee`.`users` 
 		(`firstname`, `lastname`, `email`, `password`, `company`, `phone`, `creationDate`, `isActive`, `role`) 
 	VALUES 
-		('Henry', 'Johnston', 'henry.johnston93@example.com', 'epebzVnsxm00Mzs8nfewaTBblO2EAa4cYPEl9UU9q54ixuYgeo6/Bg==', 'Westheimer', '0123456789', '2018-10-29 13:07:34', '1', 'trainee'
-);
-
-INSERT INTO 
-	`pimpmytrainee`.`users` 
-		(`firstname`, `lastname`, `email`, `password`, `company`, `phone`, `creationDate`, `isActive`, `role`) 
-	VALUES 
-		('Arlene', 'Tucker', 'arlene.tucker51@example.com', 'gL+lGqf/qyjAOfmQAtgMu0ahzB/CQYK5kbL3LmjzE2jZA9hglqiQug==', 'Depaul Dr', '0123456789', '2018-10-29 13:09:08', '1', 'trainee'
-);
-
-INSERT INTO 
-	`pimpmytrainee`.`users` 
-		(`firstname`, `lastname`, `email`, `password`, `company`, `phone`, `creationDate`, `isActive`, `role`) 
-	VALUES 
-		('Hunter', 'Boyd', 'hunter.boyd92@example.com', 'hazMzj6NMdeVpratgCtJR4/3bJAfleY+hbRjhFxsiWaEauBvVid+iA==', 'Priceton Ave', '0123456789', '2018-10-29 13:09:36', '1', 'trainee'
-);
-
-INSERT INTO 
-	`pimpmytrainee`.`users` 
-		(`firstname`, `lastname`, `email`, `password`, `company`, `phone`, `creationDate`, `isActive`, `role`) 
-	VALUES 
-		('Bonnie', 'Gregory', 'bonnie.gregory50@example.com', 'DltyRJTlPysp4WdTGJRRVOObvKSU21QbOKkpE6be0RZ9YUhocXLKmA==', 'Rudder Rd', '0123456789', '2018-10-29 13:10:09', '1', 'trainee'
-);
-
-INSERT INTO 
-	`pimpmytrainee`.`users` 
-		(`firstname`, `lastname`, `email`, `password`, `company`, `phone`, `creationDate`, `isActive`, `role`) 
-	VALUES 
-		('Marshall', 'Turner', 'marshall.turner71@example.com', '3ItvhWg805XGFWC1ZqIJ5CjDynjMmr8cWuqZKTRXO3bOLrLQuP9mVQ==', 'E Sandy Lake Rd', '0123456789', '2018-10-29 13:10:50', '1', 'trainee'
-);
+		('Henry', 'Johnston', 'henry.johnston93@example.com', 'epebzVnsxm00Mzs8nfewaTBblO2EAa4cYPEl9UU9q54ixuYgeo6/Bg==', 'Westheimer', '0123456789', '2018-10-29 13:07:34', '1', 'trainee'),
+		('Arlene', 'Tucker', 'arlene.tucker51@example.com', 'gL+lGqf/qyjAOfmQAtgMu0ahzB/CQYK5kbL3LmjzE2jZA9hglqiQug==', 'Depaul Dr', '0123456789', '2018-10-29 13:09:08', '1', 'trainee'),
+		('Hunter', 'Boyd', 'hunter.boyd92@example.com', 'hazMzj6NMdeVpratgCtJR4/3bJAfleY+hbRjhFxsiWaEauBvVid+iA==', 'Priceton Ave', '0123456789', '2018-10-29 13:09:36', '1', 'trainee'),
+		('Bonnie', 'Gregory', 'bonnie.gregory50@example.com', 'DltyRJTlPysp4WdTGJRRVOObvKSU21QbOKkpE6be0RZ9YUhocXLKmA==', 'Rudder Rd', '0123456789', '2018-10-29 13:10:09', '1', 'trainee'),
+		('Marshall', 'Turner', 'marshall.turner71@example.com', '3ItvhWg805XGFWC1ZqIJ5CjDynjMmr8cWuqZKTRXO3bOLrLQuP9mVQ==', 'E Sandy Lake Rd', '0123456789', '2018-10-29 13:10:50', '1', 'trainee');
