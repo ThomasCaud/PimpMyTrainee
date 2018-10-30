01c97f18dba12102d3f3077a10393e70bb72d4c7
Maxime LUCAS <maxime.lucas@equensworldline.com>
2018-10-30 16:24:45 +0100

    [MLS] Script SQL pour ajouter des thèmes

---
+++ src/sql/2018-10-30_5_Ajout de thèmes.sql
@@ -0,0 +1,11 @@
+INSERT INTO 
+	`pimpmytrainee`.`themes` 
+		(`label`) 
+	VALUES 
+		("Java EE"),
+		("SQL"),
+		("Docker"),
+		("Python"),
+		("Communication professionnelle"),
+		("Gestion de projets"),
+		("Mathématiques")
