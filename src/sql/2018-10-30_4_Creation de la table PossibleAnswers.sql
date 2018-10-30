1b3d093f39dbab1e57320d3a6a387c6ef44691f5
Thomas Caudrelier <tcaudrelier@horizontalsoftware.com>
2018-10-30 14:18:17 +0100

    [Scripts]Modification des valeurs par defauts

--- src/sql/2018-10-30_4_Creation de la table PossibleAnswers.sql
+++ src/sql/2018-10-30_4_Creation de la table PossibleAnswers.sql
@@ -3,9 +3,9 @@ CREATE TABLE `possibleanswers` (
   	`label` varchar(255) NOT NULL,
   	`isCorrect` tinyint(1) NOT NULL,
   	`isActive` tinyint(1) NOT NULL,
-  	`position` int NOT NULL DEFAULT 1,
+  	`position` int NOT NULL DEFAULT -1,
   	`question` bigint(20) unsigned NOT NULL,
-  	
+
 	PRIMARY KEY (`id`),
 	FOREIGN KEY (question) REFERENCES Questions(id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
