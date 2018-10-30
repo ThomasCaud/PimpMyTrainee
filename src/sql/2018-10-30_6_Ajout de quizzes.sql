6d5618b2e9929daa08d70a3051077aa0d47c46bd
Maxime LUCAS <maxime.lucas@equensworldline.com>
2018-10-30 16:44:08 +0100

    [MLS] Script SQL pour ajouter des quizzes

---
+++ src/sql/2018-10-30_6_Ajout de quizzes.sql
@@ -0,0 +1,8 @@
+INSERT INTO quizzes (title,theme,creator,creationDate,isActive) VALUES
+    ("Les nombres complexes",7,1,NOW(),1),
+    ("La dérivation",7,1,NOW(),0),
+    ("Parler à son supérieur",5,2,NOW(),1),
+    ("Les servlets",1,2,NOW(),0),
+    ("Chiffrer un projet informatique",6,2,NOW(),1),
+    ("Les jointures",2,2,NOW(),1),
+    ("Les procédures stockées",2,1,NOW(),1);
