6ee51b404aa11b7f799ee6a849e21bf1f02dfca1
Maxime LUCAS <maxime.lucas96@gmail.com>
2018-11-13 09:24:34 +0100

    [MLS] Init the startQuiz view

---
+++ WebContent/WEB-INF/start_quiz.jsp
@@ -0,0 +1,11 @@
+<%@ include file="header.jsp"%>
+
+<%@ include file="trainee_navbar.jsp"%>
+
+<div class="container">
+
+	${quiz}
+	
+</div>
+
+<%@ include file="footer.jsp"%>
---
+++ src/controllers/StartQuizController.java
@@ -0,0 +1,43 @@
+package controllers;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import common.Config;
+import models.beans.E_Role;
+import models.beans.User;
+
+@WebServlet("/" + Config.URL_START_QUIZ + "/*")
+public class StartQuizController extends ConnectedOnlyServlet {
+    private static final long serialVersionUID = 1L;
+    private static final String VIEW = "/WEB-INF/start_quiz.jsp";
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	checkSessionUser(request, response);
+
+	String pathInfo = request.getPathInfo();
+
+	if (pathInfo == null || pathInfo.isEmpty()) {
+	    response.sendError(HttpServletResponse.SC_BAD_REQUEST);
+	    return;
+	}
+
+	String urlId = request.getPathInfo().substring(1, request.getPathInfo().length());
+	Integer id;
+
+	try {
+	    id = Integer.parseInt(urlId);
+	} catch (NumberFormatException e) {
+	    response.sendError(HttpServletResponse.SC_BAD_REQUEST);
+	    return;
+	}
+
+	this.getServletContext().getRequestDispatcher(VIEW).forward(request, response);
+    }
+}
