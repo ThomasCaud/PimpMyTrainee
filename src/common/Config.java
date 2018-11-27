fe56738b35b4a25e8624d29fb9011abd6a8a0d8d
Maxime LUCAS <maxime.lucas96@gmail.com>
2018-11-27 08:22:34 +0100

    [MLS] Add an URL and a session attribute in Config class

--- src/common/Config.java
+++ src/common/Config.java
@@ -1,24 +1,26 @@
 package common;
 
 public class Config {
-    public static final String ATT_SESSION_USER = "sessionUser";
-    public static final String ATT_SESSION_QUIZ = "sessionQuiz";
-    public static final String CONF_DAO_FACTORY = "daofactory";
-    public static final Integer NB_USERS_PER_PAGE = 3;
-    public static final Integer NB_QUIZZES_PER_PAGE = 3;
+	public static final String ATT_SESSION_USER = "sessionUser";
+	public static final String ATT_SESSION_QUIZ = "sessionQuiz";
+	public static final String ATT_SESSION_CONTEXT_ID = "sessionContextID";
+	public static final String CONF_DAO_FACTORY = "daofactory";
+	public static final Integer NB_USERS_PER_PAGE = 3;
+	public static final Integer NB_QUIZZES_PER_PAGE = 3;
 
-    // URLS
-    public static final String URL_ROOT = "";
-    public static final String URL_LOGIN = "login";
-    public static final String URL_LOGOUT = "logout";
-    public static final String URL_USERS = "users";
-    public static final String URL_VIEW_USER = "viewUser";
-    public static final String URL_REGISTER_USER = "registerUser";
-    public static final String URL_QUIZZES = "quizzes";
-    public static final String URL_CREATE_QUIZ = "createQuiz";
-    public static final String URL_VIEW_QUIZ = "viewQuiz";
-    public static final String URL_START_QUIZ = "startQuiz";
-    public static final String URL_CREATE_THEME = "createTheme";
+	// URLS
+	public static final String URL_ROOT = "";
+	public static final String URL_LOGIN = "login";
+	public static final String URL_LOGOUT = "logout";
+	public static final String URL_USERS = "users";
+	public static final String URL_VIEW_USER = "viewUser";
+	public static final String URL_REGISTER_USER = "registerUser";
+	public static final String URL_QUIZZES = "quizzes";
+	public static final String URL_CREATE_QUIZ = "createQuiz";
+	public static final String URL_VIEW_QUIZ = "viewQuiz";
+	public static final String URL_START_QUIZ = "startQuiz";
+	public static final String URL_RUN_QUIZ = "runQuiz";
+	public static final String URL_CREATE_THEME = "createTheme";
 	public static final String URL_RESULTS = "records";
 	public static final String URL_VIEW_RESULT = "viewRecords";
 }
