33c3b127d7841fe3dd795a90ee70e1587a9b3d05
Maxime LUCAS <maxime.lucas96@gmail.com>
2018-11-30 17:00:15 +0100

    [MLS] Indentation

--- src/models/forms/CreateThemeForm.java
+++ src/models/forms/CreateThemeForm.java
@@ -7,40 +7,40 @@ import models.beans.Theme;
 
 public class CreateThemeForm extends AbstractForm {
 
-    // Variables that represents each field of the form
-    private static final String FIELD_LABEL = "label";
-    private ThemeDAO themeDAO;
+	// Variables that represents each field of the form
+	private static final String FIELD_LABEL = "label";
+	private ThemeDAO themeDAO;
 
-    public CreateThemeForm(ThemeDAO themeDAO) {
-	super();
-	this.themeDAO = themeDAO;
-    }
-
-    public void processLabelValidation(String label, Theme theme) {
-	if (isNullOrEmpty(label)) {
-	    setError(FIELD_LABEL, "The label is empty.");
+	public CreateThemeForm(ThemeDAO themeDAO) {
+		super();
+		this.themeDAO = themeDAO;
 	}
-	theme.setLabel(label);
-    }
 
-    // Main method called by the servlet to process the registration
-    public Theme createTheme(HttpServletRequest request) {
-	String label = getFieldValue(request, FIELD_LABEL);
+	public void processLabelValidation(String label, Theme theme) {
+		if (isNullOrEmpty(label)) {
+			setError(FIELD_LABEL, "The label is empty.");
+		}
+		theme.setLabel(label);
+	}
 
-	Theme theme = new Theme();
+	// Main method called by the servlet to process the registration
+	public Theme createTheme(HttpServletRequest request) {
+		String label = getFieldValue(request, FIELD_LABEL);
 
-	processLabelValidation(label, theme);
+		Theme theme = new Theme();
 
-	if (this.getErrors().isEmpty()) {
-	    Theme existingTheme = themeDAO.find("label", label);
+		processLabelValidation(label, theme);
 
-	    if (existingTheme != null) {
-		setError(FIELD_LABEL, "This theme already exists.");
+		if (this.getErrors().isEmpty()) {
+			Theme existingTheme = themeDAO.find("label", label);
+
+			if (existingTheme != null) {
+				setError(FIELD_LABEL, "This theme already exists.");
+				return theme;
+			}
+
+			themeDAO.createTheme(theme);
+		}
 		return theme;
-	    }
-
-	    themeDAO.createTheme(theme);
 	}
-	return theme;
-    }
 }
