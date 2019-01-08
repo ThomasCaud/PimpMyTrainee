897a0bb36f2114c7f203ef21c265e100803c7ff2
Thomas Caudrelier <thomas.caudrelier@outlook.fr>
2019-01-08 09:41:09 +0100

    Finalize beans javadoc

--- src/models/beans/RecordAnswer.java
+++ src/models/beans/RecordAnswer.java
@@ -2,35 +2,72 @@ package models.beans;
 
 import java.io.Serializable;
 
+/**
+ * Bean that represents a RecordAnswer
+ * 
+ * @author Thomas
+ *
+ */
 public class RecordAnswer implements Serializable {
 	private static final long serialVersionUID = 1L;
 	private int recordId;
 	private int answerId;
 
+	/**
+	 * Default constructor
+	 */
 	public RecordAnswer() {
 		super();
 		this.recordId = -1;
 		this.answerId = -1;
 	}
 
+	/**
+	 * Constructor with parameters
+	 * 
+	 * @param record
+	 * @param answer
+	 */
 	public RecordAnswer(int record, int answer) {
 		super();
 		this.recordId = record;
 		this.answerId = answer;
 	}
 
+	/**
+	 * get recordId
+	 * 
+	 * @return recordId
+	 */
 	public int getRecordId() {
 		return recordId;
 	}
 
+	/**
+	 * set recordId
+	 * 
+	 * @param record
+	 *            of type int
+	 */
 	public void setRecordId(int record) {
 		this.recordId = record;
 	}
 
+	/**
+	 * get answerId
+	 * 
+	 * @return answerId
+	 */
 	public int getAnswerId() {
 		return answerId;
 	}
 
+	/**
+	 * set answerId
+	 * 
+	 * @param answer
+	 *            of type int
+	 */
 	public void setAnswerId(int answer) {
 		this.answerId = answer;
 	}
